4e3cd0e70d1e65e80a5d5e76c79e0b53ec079263
Siddhi <Siddhi@DESKTOP-S6UFUN4>
2016-10-25 00:36:42 -0600

    Banker and Property classes

---
+++ CS414_Monopoly/src/Banker.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Banker {
+	private ArrayList<Player> playerList;
+	private ArrayList<Property> propertyList;
+	private ArrayList<Property> mortPropList;
+	private int availHouses;
+	private int availHotels;
+	private int availCash;
+	public ArrayList<Player> getPlayerList() {
+		return playerList;
+	}
+	public void setPlayerList(ArrayList<Player> playerList) {
+		this.playerList = playerList;
+	}
+	public ArrayList<Property> getPropertyList() {
+		return propertyList;
+	}
+	public void setPropertyList(ArrayList<Property> propertyList) {
+		this.propertyList = propertyList;
+	}
+	public int getAvailHouses() {
+		return availHouses;
+	}
+	public void setAvailHouses(int availHouses) {
+		this.availHouses = availHouses;
+	}
+	public int getAvailHotels() {
+		return availHotels;
+	}
+	public void setAvailHotels(int availHotels) {
+		this.availHotels = availHotels;
+	}
+	public int getAvailCash() {
+		return availCash;
+	}
+	public void setAvailCash(int availCash) {
+		this.availCash = availCash;
+	}
+	
+	public void mortgageProperty(Property p)
+	{
+		this.mortPropList.add(p);
+	}
+	
+	public void unmortgageProperty(Property p)
+	{
+		this.mortPropList.remove(p);
+	}
+	
+	public void updateAvailableHotels(int hotelCount)
+	{
+		this.availHotels-=hotelCount;
+	}
+	
+	public void updateAvailableHouse(int houseCount)
+	{
+		this.availHouses-=houseCount;
+	}
+	
+	
+}
