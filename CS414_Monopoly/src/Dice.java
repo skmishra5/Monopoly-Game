540158d2d6090dffe58633599514d29b37c0e224
shivani4 <shivanidave4@gmail.com>
2016-10-26 14:42:44 -0600

    Updated GUI and Dice

---
+++ CS414_Monopoly/src/Dice.java
@@ -0,0 +1,5 @@
+
+public class Dice {
+	private int diceVal1;
+	private int diceVal2;
+}
---
+++ CS414_Monopoly/src/MonopolyGroup.java
@@ -0,0 +1,11 @@
+
+public enum MonopolyGroup {
+	DARKPURPLE,
+	LIGHTBLUE,
+	LIGHTPURPLE,
+	ORANGE,
+	RED,
+	YELLOW,
+	GREEN,
+	DARKBLUE
+}
