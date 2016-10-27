8f900e69633b965fcff65b78fbc1a757f63efae3
Siddhi <Siddhi@DESKTOP-S6UFUN4>
2016-10-27 13:16:16 -0600

    Adding test class for Block

---
+++ CS414_Monopoly/src/BlockTest.java
@@ -0,0 +1,70 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BlockTest {
+	private String testdescription;
+	private PropUtil testpropUtil;
+
+	@Before
+	public void setup(){
+		
+	}
+	/**
+	 * Test method for {@link Block#Block(java.lang.String, PropUtil)}.
+	 */
+	@Test
+	public void testBlock() {
+		
+	}
+
+	/**
+	 * Test method for {@link Block#getImage()}.
+	 */
+	@Test
+	public void testGetImage() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Block#setImage(java.awt.Image)}.
+	 */
+	@Test
+	public void testSetImage() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Block#getDescription()}.
+	 */
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Block#setDescription(java.lang.String)}.
+	 */
+	@Test
+	public void testSetDescription() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Block#getPropUtil()}.
+	 */
+	@Test
+	public void testGetPropUtil() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Block#setPropUtil(PropUtil)}.
+	 */
+	@Test
+	public void testSetPropUtil() {
+		fail("Not yet implemented");
+	}
+
+}
