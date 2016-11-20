552249a38949259b3b12e0cf2d4a5ef344abebad
siddhijagtap <siddhijagtap3@gmail.com>
2016-11-19 23:27:50 -0700

    Modified the Monopoly Group

---
+++ Monopolycs414k/cs414.a5.k/cs414/a5/k/model/MonopolyGroup.java
@@ -0,0 +1,5 @@
+package cs414.a5.k.model;
+
+public enum MonopolyGroup {
+	DARKPURPLE, LIGHTBLUE, LIGHTPURPLE, ORANGE, RED, YELLOW, GREEN, DARKBLUE
+}
---
+++ Monopolycs414k/cs414.a5.k/cs414/a5/k/model/TitleDeedCardRent.java
@@ -0,0 +1,63 @@
+package cs414.a5.k.model;
+
+import java.io.Serializable;
+
+public class TitleDeedCardRent implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private int oneHouse;
+	private int twoHouse;
+	private int threeHouse;
+	private int fourHouse;
+	private int hotel;
+
+	public TitleDeedCardRent(int oneHouse, int twoHouse, int threeHouse, int fourHouse, int hotel) {
+		super();
+		this.oneHouse = oneHouse;
+		this.twoHouse = twoHouse;
+		this.threeHouse = threeHouse;
+		this.fourHouse = fourHouse;
+		this.hotel = hotel;
+	}
+
+	public int getOneHouse() {
+		return oneHouse;
+	}
+
+	public void setOneHouse(int oneHouse) {
+		this.oneHouse = oneHouse;
+	}
+
+	public int getTwoHouse() {
+		return twoHouse;
+	}
+
+	public void setTwoHouse(int twoHouse) {
+		this.twoHouse = twoHouse;
+	}
+
+	public int getThreeHouse() {
+		return threeHouse;
+	}
+
+	public void setThreeHouse(int threeHouse) {
+		this.threeHouse = threeHouse;
+	}
+
+	public int getFourHouse() {
+		return fourHouse;
+	}
+
+	public void setFourHouse(int fourHouse) {
+		this.fourHouse = fourHouse;
+	}
+
+	public int getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(int hotel) {
+		this.hotel = hotel;
+	}
+
+}
